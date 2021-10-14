163110a0fdf192f640b8cd6f96fa8f90695289de
Fernando Malaman <contato.fernandom@gmail.com>
2021-10-13 23:34:07 -0300

    fetching data

--- src/Table.js
+++ src/Table.js
@@ -1,5 +1,18 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function Table() {
+  const [posts, setPosts] = useState([]);
+
+  const fetchUrl = "https://jsonplaceholder.typicode.com/posts";
+  useEffect(() => {
+    async function fetchData() {
+      const data = await axios.get(fetchUrl);
+      setPosts(data.data);
+    }
+    fetchData();
+  }, [fetchUrl]);
+
   return (
     <table className="table table-responsive border-primary table-bordered">
       <thead>
