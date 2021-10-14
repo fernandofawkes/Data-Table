ed0c022b87b1c8a044861328d3887a490924dd58
Fernando Malaman <contato.fernandom@gmail.com>
2021-10-14 07:28:04 -0300

    add post button and popup

--- src/Table.js
+++ src/Table.js
@@ -14,6 +14,14 @@ function Table() {
   }, [fetchUrl]);
 
   return (
+    <div>
+    
+    <div className="d-flex flex-row">
+      <button type="button" className="me-3 btn btn-primary ml-auto d-block mb-2" data-bs-toggle="modal" data-bs-target="#addModalForm">
+        Add Data +
+      </button>
+    </div>
+    
     <table className="table table-responsive border-primary table-bordered">
       <thead>
         <tr>
@@ -37,7 +45,57 @@ function Table() {
         
       </tbody>
     </table>
-  );
+    {/*Add Modal */}
+      <div className="modal fade" id="addModalForm" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">Add New Post</h5>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body">
+              <form>
+                <div className="mb-3">
+                  <label className="form-label">User ID</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="userId"
+                    required
+                    disabled
+                  />
+                </div>
+                <div className="mb-3">
+                  <label className="form-label">Title</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="title"
+                    placeholder="title"
+                    required
+                  />
+                </div>
+                <div className="mb-3">
+                  <label className="form-label">Body</label>
+                  <textarea
+                    rows="4"
+                    cols="50"
+                    className="form-control"
+                    name="body"
+                    placeholder="body"
+                    required
+                  ></textarea>
+                </div>
+                <div className="modal-footer d-block">
+                  <button type="submit" data-bs-dismiss="modal" className="btn btn-warning float-end">Submit</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    );
 }
 
 export default Table;
