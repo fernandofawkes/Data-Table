3c5443b5950b1aeaae48b6b6e11123c6fe82f9c2
Fernando Malaman <p1200137@brasilprev.com.br>
2021-10-18 15:00:23 -0300

    Editing post

--- src/Table.js
+++ src/Table.js
@@ -9,7 +9,14 @@ function Table() {
     title: '',
     body : ''
   });
-   
+  
+  const [editPost, setEditPostFields] = useState({
+    userId: 0,
+    id: 0,
+    title: '',
+    body : ''
+  });
+
   const handleFormInput = (field) => (evt) => {
     evt.preventDefault();
     setNewPostFields({...newPost, [field]: evt.target.value});
@@ -27,6 +34,20 @@ function Table() {
     })
   }
 
+  const handleEditInput = (field) => (evt) => {
+    evt.preventDefault();
+    setEditPostFields({...editPost, [field]: evt.target.value});
+  }
+
+  const handleEditSubmit = (evt) => {
+    evt.preventDefault();
+
+    const newPosts = [...posts];
+    const formIndex = posts.findIndex((post) => post.id === editPost.id);
+    newPosts[formIndex] = editPost;
+    setPosts(newPosts);
+  }
+  
   const fetchUrl = "https://jsonplaceholder.typicode.com/posts";
   useEffect(() => {
     async function fetchData() {
@@ -62,7 +83,13 @@ function Table() {
           <td>{post.id}</td>
           <td>{post.title}</td>
           <td>{post.body}</td>
-          <td>Edit</td>
+          <td>
+            <button type="button" className="me-3 btn btn-primary ml-auto d-block mb-2" data-bs-toggle="modal" data-bs-target="#editModalForm"
+              onClick={(e) => setEditPostFields(post)}
+            >
+              Edit
+            </button>
+          </td>
         </tr>
       )}
         
@@ -111,6 +138,61 @@ function Table() {
                     placeholder="body"
                     required
                     onChange={handleFormInput('body')}
+                  ></textarea>
+                </div>
+                <div className="modal-footer d-block">
+                  <button type="submit" data-bs-dismiss="modal" className="btn btn-warning float-end">Submit</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    {/* Edit Modal */}
+      <div className="modal fade" id="editModalForm" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">Edit Post</h5>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body">
+              <form onSubmit={handleEditSubmit}>
+                <div className="mb-3">
+                  <label className="form-label">User ID</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="userId"
+                    required
+                    value={editPost.userId}
+                    disabled
+                    onChange={handleEditInput('userId')}
+                  />
+                </div>
+                <div className="mb-3">
+                  <label className="form-label">Title</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="title"
+                    placeholder="title"
+                    value={editPost.title}
+                    required
+                    onChange={handleEditInput('title')}
+                  />
+                </div>
+                <div className="mb-3">
+                  <label className="form-label">Body</label>
+                  <textarea
+                    rows="4"
+                    cols="50"
+                    className="form-control"
+                    name="body"
+                    placeholder="body"
+                    required
+                    value={editPost.body}
+                    onChange={handleEditInput('body')}
                   ></textarea>
                 </div>
                 <div className="modal-footer d-block">
